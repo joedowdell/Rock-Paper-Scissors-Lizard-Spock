5a67a929b2eaa719b314c38ea95b23dc66f488fe
Joe Dowdell <joedowdell@gmail.com>
2014-08-04 16:16:48 +0100

    Passes Jasmine tests

--- javascript/lib/RockPaperScissors.js
+++ javascript/lib/RockPaperScissors.js
@@ -1,3 +1,5 @@
+
+
 function Player(name) {
   // 'initialize' method goes here!
   this.name = name;
@@ -12,6 +14,22 @@ function Game(player1, player2) {
   this.player2 = player2;
 };
 
-Game.prototype.winner = function() {
-  return this.player1;
+Game.prototype.PAIRS = {
+	rock: 		{ beats: "scissors" },
+	paper: 		{ beats: "rock"},
+	scissors: { beats: "paper"}
+};
+
+Game.prototype.winner = function() { 
+
+	if(this.player1.pick === this.player2.pick) {
+	return null
+	}
+
+ 	else if (this.PAIRS[this.player1.pick].beats === this.player2.pick) {
+    return this.player1;
+  }
+  else {
+    return this.player2;
+  }
 };
