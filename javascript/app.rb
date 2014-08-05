5558bd89d744f9ef1c0d51caf6e1a1035cb3ddff
Joe Dowdell <joedowdell@gmail.com>
2014-08-05 19:12:46 +0100

    setup sinatra app

---
+++ javascript/app.rb
@@ -0,0 +1,8 @@
+require 'sinatra'
+
+
+set :public_folder, Proc.new { File.join(root, "public") }
+
+get '/' do
+  erb :index
+end
---
+++ javascript/public/javascript/interface.js
@@ -0,0 +1,19 @@
+$(document).ready(function(){
+
+	var player1 = new Player('player');
+	var player2 = new Player('computer');
+	var game = new Game(player1, player2);
+
+	$('.choices img').on('click', function(){
+	player1.picks($(this).data('pick'));
+	player2.picks(['rock', 'paper', 'scissors', 'lizard', 'spock'][Math.round((Math.random())*2)]);
+
+		$("<li>" + game.victoryMessage(game.winner(), game.loser(game.winner()), game.verb) + "</li>").prependTo('#results').slideDown();
+
+			$("#results li:gt(3)").fadeOut(function(){
+			$(this).remove();
+			});
+
+	});
+
+})
